b589aa433dc7e4742e2ec0a5861fe40834ca3422
Dev-Aaminah <aminakanwal12345@gmail.com>
2024-06-25 22:54:07 +0500

    added assignment 02

---
+++ Ex_BitwiseOperators.py
@@ -0,0 +1,31 @@
+# AND Operation Problem:
+# Calculate the result of the bitwise AND operation on the numbers 25 and 30.
+x=25
+y=30
+print(x&y)
+
+# OR Operation Problem:
+# Calculate the result of the bitwise OR operation on the numbers 45 and 25.
+num1=45
+num2=25
+print(num1|num2)
+
+# XOR Operation Problem:
+# Calculate the result of the bitwise XOR operation on the numbers 29 and 15.
+xor_var1=29
+xor_var2=15
+print(xor_var1^xor_var2)
+
+# NOT Operation Problem:
+# Calculate the result of the bitwise NOT operation on the number 42.
+number = 42
+print(~number)
+
+# Shift Operations Problem:
+# Calculate the result of left shifting the number 15 by 3 positions.
+# Calculate the result of right shifting the number 200 by 4 positions.
+
+# Left Shift (<<)
+print(15<<3)
+# Right Shift (>>)
+print(200>>4)
