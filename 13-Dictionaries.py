3e5453a95ab4ddec6358a476e208bcfaa110eeae
Dev-Aaminah <aminakanwal12345@gmail.com>
2024-07-19 22:02:25 +0500

    dictionary in python

---
+++ 13-Dictionaries.py
@@ -0,0 +1,21 @@
+person = {
+    "name": "Amina",
+    "age": 27,
+    "height": 42.5
+}
+print(person)
+print(person["height"])
+print(len(person))  # length starts with 1 not 0.
+
+
+user = {
+    "name": "Amber",
+    123: 'hello',
+    (1,2): "none",
+    "dic": {
+        "1": "First",
+        "2": "Second"
+    }
+}
+print(user)
+
